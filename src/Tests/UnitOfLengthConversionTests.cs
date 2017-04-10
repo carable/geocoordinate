c1a422b738118fd16fd80da9b2218a17772c2f8d
Jussi <jussi.svensson@dynabyte.se>
2017-04-10 11:40:54 +0200

    Add test

--- src/Tests/UnitOfLengthConversionTests.cs
+++ src/Tests/UnitOfLengthConversionTests.cs
@@ -21,10 +21,18 @@ namespace GeoCoordinatePortableTests
         }
 
         [Fact]
+        public void MeterAndKilometers()
+        {
+            Assert.Equal(1000.Meters(), 1.Kilometers().ConvertTo(UnitOfLength.Meter));
+            Assert.Equal(1.Kilometers(), 1000.Meters().ConvertTo(UnitOfLength.Kilometer));
+        }
+
+        [Fact]
         public void SameToSame()
         {
             Assert.Equal(10.0.Kilometers(), 10.Kilometers().ConvertTo(UnitOfLength.Kilometer));
             Assert.Equal(1.0.InternationalMiles(), 1.InternationalMiles().ConvertTo(UnitOfLength.InternationalMile));
+            Assert.Equal(50.0.Meters(), 50.Meters().ConvertTo(UnitOfLength.Meter));
         }
     }
 }
