6088af7bb9507a4dcf3a8bab23b2d36ebba5aa37
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2018-06-08 11:20:18 +0200

    Adding more extension methods (#3)
    * Adding more conversions
    
    * Version

---
+++ src/Tests/UnitOfLengthStringsTests.cs
@@ -0,0 +1,42 @@
+﻿using Carable.GeoCoordinates;
+using Xunit;
+
+namespace GeoCoordinatePortableTests
+{
+    public class UnitOfLengthStringsTests
+    {
+        [Theory,
+         InlineData(UnitOfLength.Inch),
+         InlineData(UnitOfLength.InternationalMile),
+         InlineData(UnitOfLength.Kilometer),
+         InlineData(UnitOfLength.Meter),
+         InlineData(UnitOfLength.Millimeter),
+        ]
+        public void Should_render_GetUSEnglishNamePluralized(UnitOfLength unitOfLength)
+        {
+            Assert.NotEmpty(unitOfLength.GetUSEnglishNamePluralized());
+        }
+        [Theory,
+         InlineData(UnitOfLength.Inch),
+         InlineData(UnitOfLength.InternationalMile),
+         InlineData(UnitOfLength.Kilometer),
+         InlineData(UnitOfLength.Meter),
+         InlineData(UnitOfLength.Millimeter),
+        ]
+        public void Should_render_GetUSEnglishName(UnitOfLength unitOfLength)
+        {
+            Assert.NotEmpty(unitOfLength.GetUSEnglishName());
+        }
+        [Theory,
+         InlineData(UnitOfLength.Inch),
+         InlineData(UnitOfLength.InternationalMile),
+         InlineData(UnitOfLength.Kilometer),
+         InlineData(UnitOfLength.Meter),
+         InlineData(UnitOfLength.Millimeter),
+        ]
+        public void Should_render_GetEnglishAbbreviation(UnitOfLength unitOfLength)
+        {
+            Assert.NotEmpty(unitOfLength.GetEnglishAbbreviation());
+        }
+    }
+}
